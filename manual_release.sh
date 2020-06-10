6a9db7750246a5a9466cd29a6f900f48764d491b
Luke Marsden <me@lukemarsden.net>
2020-06-10 21:16:32 +0100

    A manual release script for now.

---
+++ manual_release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# For when times are hard, and you don't have CI wired up yet.
+# Log into quay.io first.
+
+export REPOSITORY=quay.io/dotmesh/
+export DOCKER_TAG=release-0.8.3
+export VERSION=release-0.8.3
+
+make release_all
