fe8cf05aacf66994c4eb3d2730d7b4405767c213
Luke Marsden <me@lukemarsden.net>
2020-06-11 10:12:44 +0100

    oops, wrong release

--- manual_release.sh
+++ manual_release.sh
@@ -4,7 +4,7 @@
 # Log into quay.io first.
 
 export REPOSITORY=quay.io/dotmesh/
-export DOCKER_TAG=release-0.8.3
-export VERSION=release-0.8.3
+export DOCKER_TAG=release-0.8.2
+export VERSION=release-0.8.2
 
 make release_all
