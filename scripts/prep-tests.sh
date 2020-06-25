5e11078d79f70d0339f98f8bb7e38be1e9ae9e9a
Luke Marsden <me@lukemarsden.net>
2020-06-25 10:25:45 +0100

    Don't forget to push things

--- scripts/prep-tests.sh
+++ scripts/prep-tests.sh
@@ -6,9 +6,9 @@ export DOCKER_TAG=latest
 export CI_REGISTRY="$(hostname).local:80"
 export CI_REPOSITORY="dotmesh"
 export REPOSITORY="${CI_REGISTRY}/${CI_REPOSITORY}/"
-make build_server
-make build_operator
-make build_provisioner
+make build_server && make push_server
+make build_operator && make push_operator
+make build_provisioner && make push_provisioner
 make build_client
-make build_dind_prov
+make build_dind_prov && make push_dind_prov
 make build_dind_flexvolume
